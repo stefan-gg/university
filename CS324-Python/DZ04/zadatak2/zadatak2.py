c36496abe6652ee40f93b6b7e88d695467fe0caa
stefan-gg <stefan.gogic01@gmail.com>
2021-11-02 01:29:20 +0100

    4. domaci

--- CS324-Python/DZ04/zadatak2/zadatak2.py
+++ CS324-Python/DZ04/zadatak2/zadatak2.py
@@ -3,18 +3,19 @@ n = int(input("Unesite broj clana čiju vrednost želite da vidite : "))
 broj1 = 0
 broj2 = 1
 sum = 0
-#0 1 1 2 3 5 8
 for i in range(0, n):
     sum = broj1 + broj2
     broj1, broj2 = broj2, sum
     
-#print(sum)
-
+#1. rekurzivno resenje
 def fibonacci(a,b,n):
     if(n <= 1):
         print(a)
         return a
     return fibonacci(b,a+b,n-1)
+#2. resenje zadatka rekurzivnim postupkom
+#def fibonacciRek(n):
+#    return n if n <= 1 else fibonacciRek(n - 1) + fibonacciRek(n - 2)
 
 a = fibonacci(0,1,n)
 print(a)
