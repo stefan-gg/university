22c6f9a573360976198ae174e5c3da08e1290878
stefan-gg <stefan.gogic01@gmail.com>
2022-02-28 12:13:04 +0100

    IT355 - DZ01

---
+++ IT355-Spring/src/main/java/App.java
@@ -0,0 +1,8 @@
+import java.util.UUID;
+
+public class App {
+
+    public int compareTwoNumbers(int prviBroj, int drugiBroj) {
+        return prviBroj > drugiBroj ? prviBroj : drugiBroj;
+    }
+}
