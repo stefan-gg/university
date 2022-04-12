2ede3ac7224baeb1c7c6759f2f411b05fe508b6e
stefan-gg <stefan.gogic01@gmail.com>
2022-04-12 10:55:14 +0200

    small changes

--- IT475-Blockchain/projekat/nft/web.py
+++ IT475-Blockchain/projekat/nft/web.py
@@ -8,8 +8,8 @@ print(web3.isConnected())
 
 account_1 = "0xa09Ff79B07e23139821c80E2c4A5A2092DA782F0"
 account_2 = "0x44f63E14fe2729dD5be91cF23A319773CD06386f"
-#preko privatnog ključa proveravamo dal je ok
-#da se šalje 
+# preko privatnog ključa proveravamo dal je ok
+# da se šalje 
 private_key = "3f2b512ad68e38cc6b69d0712f041c3c70643d234c3e3b6b3aa9600eda160d0d"
 
 # build transaction
@@ -25,8 +25,8 @@ transation = {
     'value': web3.toWei(1, 'ether'),
     'gas': 200000,
     'gasPrice': web3.toWei('50', 'gwei')
-    #gwei > wei manje od etheriuma
-    #nonce sprecava da se 2 puta transakcija posalje
+    # gwei > wei manje od etheriuma
+    # nonce sprečava da se 2 puta ista transakcija pošalje
 }
 
 signed_tr = web3.eth.account.signTransaction(transation, private_key)
