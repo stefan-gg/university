7bb68fc985bb802aa336e134047b42fa1c7abe59
stefan-gg <stefan.gogic01@gmail.com>
2022-04-20 11:25:48 +0200

    small changes

--- IT475-Blockchain/projekat/nft/nft-structure/nft_art.py
+++ IT475-Blockchain/projekat/nft/nft-structure/nft_art.py
@@ -1,12 +1,12 @@
 import hashlib
-
+from block_data import BlockData
 class NFTArt:
     def __init__(self, nft_name, nft_id):
         self.nft_name = nft_name
         self.nft_id = nft_id
     
-    #TODO id = hash ime_slike + ...
-    def hash_nft_data(self, block_data):
+    #hash id = hash ime_slike + ...
+    def hash_nft_data(self, block_data: BlockData):
         string_to_hash = self.nft_name + self.nft_id + block_data
         hashed_value = hashlib.sha256(string_to_hash.encode('utf-8')).hexdigest()
         return hashed_value
