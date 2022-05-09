507a8b4c8f0749f80576cb1ef65a2acf86648d76
stefan-gg <stefan.gogic01@gmail.com>
2022-05-10 01:07:53 +0200

    DZ02

--- IT475-Blockchain/domaci/DZ02/IntegerEllipticCurve.py
+++ IT475-Blockchain/domaci/DZ02/IntegerEllipticCurve.py
@@ -14,7 +14,7 @@ if (r == 0):
    print("Error: (a, b) = ("+aa+", "+bb+") and 4a^3 + 27b^2 == 0")
    exit()
 
-print("Elliptic equation: y^2 = x^3 + ax + b with (a, b) = ("
+print("Elliptic equation : y^2 = x^3 + ax + b with (a, b) = ("
    +aa+", "+bb+")")
 
 x = 0
@@ -22,12 +22,12 @@ r = x**3 + a*x + b
 while (r >= 0):
   x = x - 1
   r = x**3 + a*x + b
-print("Starting with: x = "+str(x))
+print("Starting with : x = "+str(x))
 
 while ( r<0 ):
   x = x + 1
   r = x**3 + a*x + b
-print("First candidate: x = "+str(x))
+print("First candidate : x = "+str(x))
 
 y = 0
 while (x < 100000):
@@ -39,7 +39,7 @@ while (x < 100000):
       l = y**2
 
    if (l == r):
-      print("Point on curve: (x, y) = ("+str(x)+", "+str(y)+")")
+      print("Point on curve : (x, y) = ("+str(x)+", "+str(y)+")")
 
    x = x + 1
 
