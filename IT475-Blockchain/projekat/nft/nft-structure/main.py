2c84d7bba7f160915d4ef01f41a984f01cfff1ac
unknown <jovan.joca.spasic@gmail.com>
2022-05-29 15:17:29 +0200

    PySimpleGUI added!

--- IT475-Blockchain/projekat/nft/nft-structure/main.py
+++ IT475-Blockchain/projekat/nft/nft-structure/main.py
@@ -1,4 +1,5 @@
-from os import listdir
+from asyncio.windows_events import NULL
+import os
 from PIL import Image
 from block import Block
 from nft_art import NFTArt
@@ -7,9 +8,12 @@ from block_header import BlockHeader
 from transaction import Transaction
 from block import Block
 import time
+import io
+import PySimpleGUI as sg
+import re
 
 def loadImages(path):
-    imagesList = listdir(path)
+    imagesList = os.listdir(path)
     loadedImages = []
     for image in imagesList:
         img = Image.open(path + image)
@@ -36,7 +40,7 @@ for img in images:
     if len(blockchain) != 0:
         prev_hash = blockchain[counter - 1].block_header.block_hash
     b_header = BlockHeader(prev_hash, 2, 0, b_data)
-    transaction = Transaction(owners[counter], buyers[counter], prices[counter], art, 50, 100)
+    transaction = NULL
     block = Block(b_header, transaction)
 
     blockchain.append(block)
@@ -47,11 +51,76 @@ for img in images:
 for block in blockchain:
     print(block, end='\n\n')
 
-# ******
-# ako hocemo da unosimo nasi podaci doraditi kod ispod
-# ******
-#name = input("Enter your name")
-#picture_name = input("Enter the name of the nft you would like to buy >>")
-# if picture_name in images:
-    # art = NFTArt(picture_name)
-    # b_data = BlockData()
+layout = [
+    [sg.Image(key="-IMAGE-")],
+    [
+        sg.Button("Load images"),
+        sg.Button("Previous"),
+        sg.Button("Next")
+    ],
+    [sg.Text('Price of this NFT is: ', key="-PRICE-")],
+    [
+        [sg.Text('Name', size =(7, 0)), sg.InputText()],
+        [sg.Text('Amount', size =(7, 0)), sg.InputText()],
+        [sg.Submit("Submit")]
+    ]
+]
+
+win = sg.Window("NFT blockchain", layout)
+
+img_index = 0
+
+while True:
+    event, values = win.read()
+    win["-PRICE-"].update('Price of this NFT is: ')
+    if event == sg.WIN_CLOSED:
+        break
+    if event == "Previous":
+        if img_index == 0:
+            img_index = len(images) - 1
+        else:
+            img_index -= 1
+    elif event == "Next":
+        if img_index == len(images) - 1:
+            img_index = 0
+        else:
+            img_index += 1
+
+    img = images[img_index]
+    img.thumbnail((400, 400))
+    bio = io.BytesIO()
+    img.save(bio, format="PNG")
+
+    win["-IMAGE-"].update(data=bio.getvalue())
+    price = '0 ETH'
+    for block in blockchain:
+        if block.block_header.block_data.data["nft_art"].nft_image == images[img_index]:
+            price = block.block_header.block_data.data["price"]
+            break
+    win["-PRICE-"].update(win["-PRICE-"].get() + price)
+
+    flag = False
+
+    if event == "Submit":
+        if values[0] != '' and values[1] != '':
+            NFTPrice = price.replace(" ETH", "")
+            for i in values[1]:
+                if i not in '0123456789':
+                    sg.Popup('The amount needs to be inputed as number that represents value in ETH.', keep_on_top=True)
+                    flag = False
+                    break
+                else:
+                    flag = True
+            if flag:
+                if int(NFTPrice) <= int(values[1]):
+                    
+                    #TODO: Napisati da se kupi nft i upis u blockchain
+
+                    print()
+                else:
+                    sg.Popup("Inputed amout is less then required amount.", keep_on_top=True)
+        else:
+            sg.Popup("Please input values in all fields.", keep_on_top=True)
+
+
+win.close()
