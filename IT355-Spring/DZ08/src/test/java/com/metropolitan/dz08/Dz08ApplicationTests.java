bde604a6dff410858a12bc0f4dc04408c95cb328
stefan-gg <stefan.gogic01@gmail.com>
2022-06-03 03:09:29 +0200

    Tests added - not working

--- IT355-Spring/DZ08/src/test/java/com/metropolitan/dz08/Dz08ApplicationTests.java
+++ IT355-Spring/DZ08/src/test/java/com/metropolitan/dz08/Dz08ApplicationTests.java
@@ -1,13 +1,41 @@
 package com.metropolitan.dz08;
 
+import com.metropolitan.dz08.controller.UserController;
+import com.metropolitan.dz08.model.User;
+import com.metropolitan.dz08.service.UserService;
+import org.aspectj.lang.annotation.Before;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 @SpringBootTest
 class Dz08ApplicationTests {
+    private UserService userService;
+    private UserController userController;
 
-    @Test
-    void contextLoads() {
+    @Before("")
+    public void init() {
+        userService = Mockito.mock(UserService.class);
+        userController = new UserController();
     }
 
+    @Test
+    public void showEditEmployeeForm() {
+        User user = new User();
+        user.setUsername("Stefan");
+        user.setPassword("$2a$04$W1DIGFBNXv85146useZtQ.X0e7.yHyJpN3iVhyVbEDNc7x9teBmq2");
+        user.setRole("ROLE_USER");
+
+        Mockito.when(userService.getUserByUsername("Stefan")).thenReturn(user);
+        ModelMap model = new ModelMap();
+
+        String viewName = userController.showEditEmployeeForm(1, (Model) model);
+        assertEquals(viewName, "edit_user");
+        assertEquals(model.get("user"), user);
+    }
 }
