d9ae2f1a00573e85e189bb89e68b196dbf0ed749
stefan-gg <stefan.gogic01@gmail.com>
2022-06-24 03:29:31 +0200

    Project Finished !

--- CS374-ArtificialInteligence/CS374-Project/letters.py
+++ CS374-ArtificialInteligence/CS374-Project/letters.py
@@ -29,29 +29,6 @@ print("Test data shape: ", test_x.shape)
 word_dict = {0:'A',1:'B',2:'C',3:'D',4:'E',5:'F',6:'G',7:'H',8:'I',9:'J',10:'K',11:'L',12:'M',13:'N',14:'O',15:'P',16:'Q',17:'R',18:'S',19:'T',20:'U',21:'V',22:'W',23:'X', 24:'Y',25:'Z'}
 
 
-y_int = np.int0(y)
-count = np.zeros(26, dtype='int')
-for i in y_int:
-    count[i] +=1
-alphabets = []
-for i in word_dict.values():
-     alphabets.append(i)
-fig, ax = plt.subplots(1,1, figsize=(10,10))
-ax.barh(alphabets, count)
-plt.xlabel("Number of elements :")
-plt.ylabel("Alphabets")
-plt.grid()
-plt.show()
-
-shuff = shuffle(train_x[:100])
-fig, ax = plt.subplots(3,3, figsize = (10,10))
-axes = ax.flatten()
-for i in range(9):
-    _, shu = cv2.threshold(shuff[i], 30, 200, cv2.THRESH_BINARY)
-    axes[i].imshow(np.reshape(shuff[i], (28,28)), cmap="Greys")
-plt.show()
-
-
 train_X = train_x.reshape(train_x.shape[0],train_x.shape[1],train_x.shape[2], 1)
 print("New shape of train data : ", train_X.shape)
 test_X = test_x.reshape(test_x.shape[0], test_x.shape[1], test_x.shape[2], 1)
